c7f84c3dc4572b775a76c692a83a795e203a336f
rob <robdelacruz@gmail.com>
2023-11-18 21:08:38 +0800

    various

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function create_tables($db) {
+    $db = new SQLite3("db.sqlite");
+    $db->exec("CREATE TABLE user (user_id INTEGER PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL");
+}
+
+function register_user($db, $username, $pwd) {
+    $h = password_hash($pwd, PASSWORD_DEFAULT);
+    $stmt = $db->prepare("INSERT INTO user (username, password) VALUES (?, ?)");
+    $stmt->bindParam(1, $username);
+    $stmt->bindParam(2, $h);
+    $stmt->execute();
+
+    $userid = $db->lastInsertRowID();
+}
+
+?>
